b97b85067af523214db3c402875f33e83c8fe56b
Zyta <zyta.slowianska@gmail.com>
2023-03-26 19:34:18 +0200

    feat: switch statement with fallthrough

--- Course Code/src/Section3/GradeFun.java
+++ Course Code/src/Section3/GradeFun.java
@@ -13,18 +13,23 @@ public class GradeFun {
 
         switch(grade) {
             case 'A':
+            case 'a':
                 System.out.println("Great job!");
                 break;
             case 'B':
+            case 'b':
                 System.out.println("Good job.");
                 break;
             case 'C':
+            case 'c':
                 System.out.println("You can do better.");
                 break;
             case 'D':
+            case 'd':
                 System.out.println("You're getting pretty close to failing");
                 break;
             case 'F':
+            case 'f':
                 System.out.println("You are failing the course!");
                 break;
             default:
