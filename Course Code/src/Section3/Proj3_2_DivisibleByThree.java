5fc1d6fe0369ebf27b6d113493216d9d6d86767a
Zyta <zyta.slowianska@gmail.com>
2023-04-05 17:05:15 +0200

    feat: Project 03-02: my solution

---
+++ Course Code/src/Section3/Proj3_2_DivisibleByThree.java
@@ -0,0 +1,19 @@
+package Section3;
+
+import java.util.Scanner;
+
+public class Proj3_2_DivisibleByThree {
+    public static void main (String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int userNumber;
+
+        System.out.println("Provide a number:");
+        userNumber = keyboard.nextInt();
+
+        if(userNumber%3 == 0) {
+            System.out.println(userNumber + " is divisible by 3");
+        } else {
+            System.out.println(userNumber + " is NOT divisible by 3");
+        }
+    }
+}
