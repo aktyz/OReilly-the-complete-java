1420bc326e29001de86991b473f8fa0e873ef09a
Zyta <zyta.slowianska@gmail.com>
2023-04-08 19:28:55 +0200

    feat: Project 04-01: Corrected: from ArrayList to List

--- Course Code/src/Section4/Proj4_1_Arrays.java
+++ Course Code/src/Section4/Proj4_1_Arrays.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 public class Proj4_1_Arrays {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        ArrayList<Integer> myAL = new ArrayList<>();
-        for(int i = 0; i < 5; i++) {
+        int[] myAL = new int[5];
+        for(int i = 0; i < myAL.length; i++) {
             System.out.print("Enter an integer?\t");
-            myAL.add(s.nextInt());
+            myAL[i] = s.nextInt();
         }
 
-        for(int i = 0; i < myAL.size(); i++) {
-            System.out.println("The result is: " + myAL.get(i) * 2);
+        for(int i = 0; i < myAL.length; i++) {
+            System.out.println("The result is: " + myAL[i] * 2);
         }
     }
 }
