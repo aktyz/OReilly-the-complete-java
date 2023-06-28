fbb87e952420804d3136dac1d35ee7eb2787c1c6
Zyta <zyta.slowianska@gmail.com>
2023-06-28 17:19:48 +0200

    feat: Project 05-01: reviewed solution

--- Course Code/src/Section5/Proj5_1_NamePermutations.java
+++ Course Code/src/Section5/Proj5_1_NamePermutations.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class Proj5_1_NamePermutations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] userNames = new String[5];
-        String[] names = new String[5];
-        String[] familyNames = new String[5];
-        /* Collect user input */
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Please enter name " + i);
-            userNames[i] = scanner.nextLine();
+        final int NUM_NAMES = 5;
+        StringBuilder userName;
+        String[] names = new String[NUM_NAMES];
+        String[] familyNames = new String[NUM_NAMES];
+
+        /* Collect user input and split into two different arrays*/
+        for (int i = 0; i < NUM_NAMES; i++) {
+            System.out.println("Please enter name " + i + "\t");
+            userName = new StringBuilder(scanner.nextLine());
+            int spaceIndex = userName.indexOf(" ");
+            names[i] = userName.substring(0, spaceIndex);
+            familyNames[i] = userName.substring(spaceIndex + 1);
         }
-        /* Create two arrays filled with user data */
-        for(int i = 0; i < 5; i++) {
-            int spaceIndex = userNames[i].indexOf(" ");
-            names[i] = userNames[i].substring(0, spaceIndex);
-            familyNames[i] = userNames[i].substring(spaceIndex + 1);
-        }
+
         /* Output NamePermutations */
         for(String name : names) {
             for(String familyName : familyNames) {
