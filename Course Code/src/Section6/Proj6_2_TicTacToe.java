59ed793a19962b0f1cf11d4bf3cddaa33c079cea
Zyta <zyta.slowianska@gmail.com>
2023-07-02 22:10:27 +0200

    feat: Functions initializeGameBoard() & printCurrentBoard()

--- Course Code/src/Section6/Proj6_2_TicTacToe.java
+++ Course Code/src/Section6/Proj6_2_TicTacToe.java
@@ -5,11 +5,41 @@ public class Proj6_2_TicTacToe {
         runGame();
     } // end of main()
 
-    public static void runGame() {}
+    public static void runGame() {
+        String winner = "";
+        String X = "X";
+        String O = "O";
+        String TIE = "C";
+        int ROWS = 3;
+        int COLS = 3;
+        String[][] gameBoard = new String[ROWS][COLS];
+        initializeGameBoard(gameBoard);
+        printCurrentBoard(gameBoard);
+    }
 
-    public static void initializeGameBoard(String[][] gameBoard) {}
+    public static void initializeGameBoard(String[][] gameBoard) {
+        for(int i = 0; i < gameBoard.length; i++) {
+            for(int j = 0; j < gameBoard[i].length; j++) {
+                gameBoard[i][j] = " ";
+            }
+        }
+    }
 
-    public static void printCurrentBoard(String[][] gameBoard) {}
+    public static void printCurrentBoard(String[][] gameBoard) {
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard[i].length; j++) {
+                System.out.print(gameBoard[i][j]);
+                if(j < 2) {
+                    System.out.print(" | ");
+                }
+            }
+            System.out.println();
+            if(i < 2) {
+                System.out.print("- - - - -");
+                System.out.println();
+            }
+        }
+    }
 
     public static void getUserInput(boolean isXTurn, String[][] gameBoard) {}
 
