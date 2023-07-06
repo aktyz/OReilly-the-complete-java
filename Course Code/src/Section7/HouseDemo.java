a08582fc4c9510f337ae7fa6dbc69a54f3f78dc3
Zyta <zyta.slowianska@gmail.com>
2023-07-06 21:07:56 +0200

    chore: Classes Demo - refactoring

--- Course Code/src/Section7/HouseDemo.java
+++ Course Code/src/Section7/HouseDemo.java
@@ -9,14 +9,16 @@ public class HouseDemo {
         yourHouse.setNumWindows(10);
         yourHouse.setColor("blue");
 
-        System.out.println();
-        System.out.println("Your house is " + yourHouse.getColor()
-        + " and has " + yourHouse.getNumStories() + " stories"
-        + " and " + yourHouse.getNumWindows() + " windows.");
-        System.out.println();
-        System.out.println("My house is " + myHouse.getColor()
-                + " and has " + myHouse.getNumStories() + " stories"
-                + " and " + myHouse.getNumWindows() + " windows.");
+        System.out.println("My house data:");
+        printHouseData(myHouse);
+        System.out.println("Your house data:");
+        printHouseData(yourHouse);
+    }
+
+    public static void printHouseData(House house) {
+        System.out.println("The house is " + house.getColor()
+                + " and has " + house.getNumStories() + " stories"
+                + " and " + house.getNumWindows() + " windows.");
         System.out.println();
     }
 }
