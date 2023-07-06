5e50e1c8629c346eaf7e89a0278279948c202cb2
Zyta <zyta.slowianska@gmail.com>
2023-07-06 18:41:37 +0200

    feat: Project 06-02: reviewed solution

--- Course Code/src/Section6/Proj6_2_TicTacToe.java
+++ Course Code/src/Section6/Proj6_2_TicTacToe.java
@@ -82,20 +82,20 @@ public class Proj6_2_TicTacToe {
     }
 
     public static String getWinner(String[][] gameBoard) {
-        if(gameBoard[0][0].equals(gameBoard[0][1]) && gameBoard[0][1].equals(gameBoard[0][2]))
-            return gameBoard[0][0];
-        if(gameBoard[1][0].equals(gameBoard[1][1]) && gameBoard[1][1].equals(gameBoard[1][2]))
-            return gameBoard[1][0];
-        if(gameBoard[2][0].equals(gameBoard[2][1]) && gameBoard[2][1].equals(gameBoard[2][2]))
-            return gameBoard[2][0];
+        for (int i = 0; i < ROWS; i++) {
+            if(!gameBoard[i][0].equals(" ")
+                    && gameBoard[i][0].equals(gameBoard[i][1])
+                    && gameBoard[i][1].equals(gameBoard[i][2]))
+                return gameBoard[i][0];
+        }
+        for (int i = 0; i < COLS; i++) {
+            if(!gameBoard[0][i].equals(" ")
+                    && gameBoard[0][i].equals(gameBoard[1][i])
+                    && gameBoard[1][i].equals(gameBoard[2][i]))
+                return gameBoard[0][i];
+        }
         if(gameBoard[0][0].equals(gameBoard[1][1]) && gameBoard[1][1].equals(gameBoard[2][2]))
             return gameBoard[0][0];
-        if(gameBoard[0][0].equals(gameBoard[1][0]) && gameBoard[1][0].equals(gameBoard[2][0]))
-            return gameBoard[0][0];
-        if(gameBoard[0][1].equals(gameBoard[1][1]) && gameBoard[1][1].equals(gameBoard[2][1]))
-            return gameBoard[0][1];
-        if(gameBoard[0][2].equals(gameBoard[1][2]) && gameBoard[1][2].equals(gameBoard[2][2]))
-            return gameBoard[0][2];
         if(gameBoard[0][2].equals(gameBoard[1][1]) && gameBoard[1][1].equals(gameBoard[2][0]))
             return gameBoard[0][2];
         return "";
